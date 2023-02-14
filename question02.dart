0183af49ec8e6197d87821b767b0de3f7b4e4383
diwansabir <124901998+diwansabir@users.noreply.github.com>
2023-02-14 17:21:59 +0530

    Add files via upload

---
+++ question02.dart
@@ -0,0 +1,17 @@
+class Sums{
+    var num1  = 40;
+   var num2 =15 ;
+
+
+  void display(){
+    print("addiction is : ${num1 + num2}");
+    print("Multiplication is : ${num1 * num2}");
+    print("substraction is : ${num1 - num2}");
+    print("Division is : ${num1 / num2}");
+  }
+}
+void main(){
+  var obj  = new Sums();
+  obj.display();
+
+}
